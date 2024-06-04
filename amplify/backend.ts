e70486dfca416f9d434d354c270d9ad7d75df850
fonchu <fonchu.e.venyuy@gmail.com>
2024-06-04 08:22:34 +0100

    updates

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,22 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { data, MODEL_ID, generateHaikuFunction } from "./data/resource";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { sayHello } from './functions/bedrock-lambda-fn/resource';
 
-defineBackend({
+export const backend = defineBackend({
   auth,
   data,
+  generateHaikuFunction,
+  sayHello
 });
+
+backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ["bedrock:InvokeModel"],
+    resources: [
+      `arn:aws:bedrock:*::foundation-model/${MODEL_ID}`,
+    ],
+  })
+);
